e1de4928e4f3ab1abaf80309ff965c07954f136d
Tarun <fidato.july13@gmail.com>
2024-12-16 23:41:46 +0000

    Update ldh-ctrl-sh.sh

--- ldh-ctrl-sh.sh
+++ ldh-ctrl-sh.sh
@@ -3,4 +3,4 @@
 # Get the current time
 current_time=$(date +"%H:%M:%S")
 
-echo "trn-1 Remote control from anywhere to ldh demo... at current time : $current_time"
+echo "trn-2 Remote control from anywhere to ldh demo... at current time : $current_time"
